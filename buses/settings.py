33bcacafd36868b57b275032869b809d87d2bc3e
gladegoober <glade@midlandbus.uk>
2026-06-10 00:30:46 +0100

    Update DEBUG setting to False in settings.py and adjust ptyprocess dependency marker in uv.lock

--- buses/settings.py
+++ buses/settings.py
@@ -19,7 +19,7 @@ CSRF_FAILURE_VIEW = "busstops.views.csrf_failure"
 
 TEST = "test" in sys.argv or "pytest" in sys.argv[0]
 DEBUG = bool(os.environ.get("DEBUG", False))
-DEBUG = True
+DEBUG = False
 
 DEFAULT_FROM_EMAIL = os.environ.get(
     "DEFAULT_FROM_EMAIL",
